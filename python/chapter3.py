0e69d21e381f6ec11f225370d26bdac5504d092a
RealNattawattHongthong <73651838+RealNattawattHongthong@users.noreply.github.com>
2023-03-09 18:41:49 +0700

    GG

--- python/chapter3.py
+++ python/chapter3.py
@@ -1,10 +1,10 @@
 def findmidprefix(s):
     prefixes = [s[:i] for i in range(1, len(s)+1)]
     n = len(prefixes)
-    mid_prefix_index = n // 2
+    mid_prefix_index = n // 2 + 1
     return prefixes[mid_prefix_index]
 
-s = "Hello World!"
+s = "Hello, World!"
 result = findmidprefix(s)
 print(result)
 ###
